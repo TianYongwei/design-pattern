387c42ceca79eb28ead3bf39ea989ea801f1111d
869358189@qq.com <869358189@qq.com>
2020-10-20 12:47:46 +0800

    AC

--- src/main/java/com/tianyongwei/DesignPattern/DesignPatternApplication.java
+++ src/main/java/com/tianyongwei/DesignPattern/DesignPatternApplication.java
@@ -1,4 +1,4 @@
-package com.tyw.DesignPattern;
+package com.tianyongwei.DesignPattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tianyongwei/DesignPattern/DesignPatternApplicationTests.java
+++ src/test/java/com/tianyongwei/DesignPattern/DesignPatternApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tyw.DesignPattern;
+package com.tianyongwei.DesignPattern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
